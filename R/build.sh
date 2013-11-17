046306c7de4e7754930ae2ef9dd34f2cca62c941
antagomir <antagomir@opencomp>
2013-11-17 21:50:29 +0100

    new pkg version

--- R/build.sh
+++ R/build.sh
@@ -1,4 +1,4 @@
-~/bin/R-2.15.2/bin/R CMD BATCH document.R # aja erikseen
+~/bin/R-2.15.2/bin/R CMD BATCH document.R 
 ~/bin/R-2.15.2/bin/R CMD check pkg
 ~/bin/R-2.15.2/bin/R CMD build pkg
-~/bin/R-2.15.2/bin/R CMD INSTALL NMGS_0.1.01.tar.gz
+~/bin/R-2.15.2/bin/R CMD INSTALL NMGS_0.1.02.tar.gz
