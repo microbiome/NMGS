31ab0cd45ba19bb7d204b70593e07f2abeecfddb
Leo Lahti <leo.lahti@iki.fi>
2016-04-04 11:38:24 +0200

    ok

--- R/build.sh
+++ R/build.sh
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
-
-#~/bin/R-2.15.2/bin/R CMD BATCH document.R # aja erikseen
-~/bin/R-2.15.2/bin/R CMD check pkg
-~/bin/R-2.15.2/bin/R CMD build pkg
-~/bin/R-2.15.2/bin/R CMD INSTALL NMGS_0.0.03.tar.gz
-
-=======
 /usr/bin/R CMD BATCH document.R 
 /usr/bin/R CMD check pkg
 /usr/bin/R CMD build pkg
 /usr/bin/R CMD INSTALL NMGS_0.1.03.tar.gz
->>>>>>> 34a921a77a59b00ef020fb6566f642d0394ff933
+
